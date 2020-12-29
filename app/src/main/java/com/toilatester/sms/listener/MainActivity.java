2d2cb86d3b9e80dad39338614cbfcdc73e2ffcd2
hnminh <hnminh@outlook.com>
2020-12-29 21:06:49 +0700

    feat: init

--- app/src/main/java/com/toilatester/sms/listener/MainActivity.java
+++ app/src/main/java/com/toilatester/sms/listener/MainActivity.java
@@ -1,31 +1,101 @@
 package com.toilatester.sms.listener;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.toilatester.sms.manager.ReadSMS;
+import com.toilatester.sms.models.SMSData;
 import com.toilatester.smslistener.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button startSMS, stopSMS, startServer, stopServer;
+    /**
+     * permissions request code
+     */
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+
+    /**
+     * Permissions that need to be explicitly requested from end user.
+     */
+    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[]{
+            Manifest.permission.READ_SMS, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECEIVE_SMS};
+
+    private Button startSMS, stopSMS, startServer, stopServer, fetchSMS;
+    private ReadSMS readSMS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //this.checkPermissions();
         setContentView(R.layout.activity_main);
+        this.readSMS = new ReadSMS(this.getApplicationContext(), this.getContentResolver());
+        this.startSMS = (Button) findViewById(R.id.startService);
+        this.stopSMS = (Button) findViewById(R.id.stopService);
+        this.startServer = (Button) findViewById(R.id.startServer);
+        this.stopServer = (Button) findViewById(R.id.stopServer);
+        this.fetchSMS = (Button) findViewById(R.id.fetchSMS);
 
-        startSMS = (Button) findViewById(R.id.startService);
-        stopSMS = (Button) findViewById(R.id.stopService);
-        startServer = (Button) findViewById(R.id.startServer);
-        stopServer = (Button) findViewById(R.id.stopServer);
+        this.startSMS.setOnClickListener(this);
+        this.stopSMS.setOnClickListener(this);
+        this.startServer.setOnClickListener(this);
+        this.stopServer.setOnClickListener(this);
+        this.fetchSMS.setOnClickListener(this);
+    }
 
-        startSMS.setOnClickListener(this);
-        stopSMS.setOnClickListener(this);
-        startServer.setOnClickListener(this);
-        stopServer.setOnClickListener(this);
+    protected void checkPermissions() {
+        final List<String> missingPermissions = new ArrayList<String>();
+        // check all required dynamic permissions
+        for (final String permission : REQUIRED_SDK_PERMISSIONS) {
+            final int result = ContextCompat.checkSelfPermission(this, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.add(permission);
+            }
+        }
+        if (!missingPermissions.isEmpty()) {
+            // request all missing permissions
+            final String[] permissions = missingPermissions
+                    .toArray(new String[missingPermissions.size()]);
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS);
+        } else {
+            final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
+            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
+            onRequestPermissionsResult(REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS,
+                    grantResults);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                for (int index = permissions.length - 1; index >= 0; --index) {
+                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+                        // exit the app if one permission is not granted
+                        Toast.makeText(this, "Required permission '" + permissions[index]
+                                + "' not granted, exiting", Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+                }
+                // all permissions were granted
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
@@ -38,5 +108,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startService(new Intent(this, NettyServerService.class));
         else if (view == stopServer)
             stopService(new Intent(this, NettyServerService.class));
+        else if (view == fetchSMS) {
+
+            List<SMSData> data = this.readSMS.getAllSMSMessages();
+            data.forEach(sms -> {
+                System.out.println("====================DEBUG: " + sms.getReceiveDate().toString() + " - " + sms.getMobile() + " [" + sms.getMessage() + "]");
+            });
+        }
     }
 }
--- app/src/main/java/com/toilatester/sms/manager/ReadSMS.java
+++ app/src/main/java/com/toilatester/sms/manager/ReadSMS.java
@@ -1,4 +1,56 @@
 package com.toilatester.sms.manager;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.Telephony;
+import android.widget.Toast;
+
+import com.toilatester.sms.models.SMSData;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class ReadSMS {
+
+    ContentResolver content;
+    Context context;
+
+    public ReadSMS(Context context, ContentResolver content) {
+        this.content = content;
+        this.context = context;
+    }
+
+    public List<SMSData> getAllSMSMessages() {
+        List<SMSData> smsMessages = new ArrayList<>();
+        Uri uri = Uri.parse("content://sms/inbox");
+        Cursor c = this.content.query(uri, null, null, null, null);
+        if (c == null) {
+            Toast.makeText(this.context, "No message in SMS box", Toast.LENGTH_SHORT).show();
+            return smsMessages;
+        }
+        int totalSMS = c.getCount();
+        Toast.makeText(this.context, String.format("Total %d message(s) in SMS box", totalSMS), Toast.LENGTH_SHORT).show();
+        if (c.moveToFirst()) {
+            for (int j = 0; j < totalSMS; j++) {
+                String smsDate = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.DATE));
+                String number = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.ADDRESS));
+                String body = c.getString(c.getColumnIndexOrThrow(Telephony.Sms.BODY));
+                Date date = new Date(Long.valueOf(smsDate));
+                switch (Integer.parseInt(c.getString(c.getColumnIndexOrThrow(Telephony.Sms.TYPE)))) {
+                    case Telephony.Sms.MESSAGE_TYPE_INBOX:
+                        smsMessages.add(new SMSData(number, body, date));
+                        break;
+                    case Telephony.Sms.MESSAGE_TYPE_SENT:
+                    case Telephony.Sms.MESSAGE_TYPE_OUTBOX:
+                    default:
+                        break;
+                }
+                c.moveToNext();
+            }
+        }
+        return smsMessages;
+    }
 }
