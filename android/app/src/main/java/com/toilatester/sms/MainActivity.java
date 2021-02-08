10ad18af1a730eadf032c6f5a9f05d93c051c616
hnminh <hnminh@outlook.com>
2021-02-08 21:47:42 +0700

    feat: update to foreground service

---
+++ android/app/src/main/java/com/toilatester/sms/MainActivity.java
@@ -0,0 +1,131 @@
+package com.toilatester.sms;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.toilatester.sms.listener.NettyServerService;
+import com.toilatester.smslistener.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    /**
+     * permissions request code
+     */
+    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+
+    /**
+     * Permissions that need to be explicitly requested from end user.
+     */
+    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_SMS,
+            Manifest.permission.RECEIVE_SMS,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.FOREGROUND_SERVICE};
+
+    private Button startServer, stopServer;
+    private int serverPort;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.serverPort = this.getIntent().getIntExtra("serverPort", 8181);
+        checkPermissions();
+        setContentView(R.layout.activity_main);
+        startSmsService(this.serverPort);
+        bindActionListener();
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == startServer) {
+            startService(createSmsServerIntent(this.serverPort));
+        } else if (view == stopServer) {
+            stopService(new Intent(this, NettyServerService.class));
+        }
+    }
+
+    protected void checkPermissions() {
+        final List<String> missingPermissions = new ArrayList<String>();
+        // check all required dynamic permissions
+        for (final String permission : REQUIRED_SDK_PERMISSIONS) {
+            final int result = ContextCompat.checkSelfPermission(this, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                missingPermissions.add(permission);
+            }
+        }
+        if (!missingPermissions.isEmpty()) {
+            // request all missing permissions
+            final String[] permissions = missingPermissions
+                    .toArray(new String[missingPermissions.size()]);
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS);
+        } else {
+            final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
+            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
+            onRequestPermissionsResult(REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS,
+                    grantResults);
+        }
+    }
+
+    private void startSmsService(int serverPort) {
+        Intent smsServiceIntent = createSmsServerIntent(serverPort);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(smsServiceIntent);
+        } else {
+            startService(smsServiceIntent);
+        }
+    }
+
+    private Intent createSmsServerIntent(int serverPort) {
+        Intent smsIntent = new Intent(this, NettyServerService.class);
+        smsIntent.putExtra("serverPort", serverPort);
+        return smsIntent;
+    }
+
+    private void bindActionListener() {
+        this.startServer = (Button) findViewById(R.id.startServer);
+        this.stopServer = (Button) findViewById(R.id.stopServer);
+
+        this.startServer.setOnClickListener(this);
+        this.stopServer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                for (int index = permissions.length - 1; index >= 0; --index) {
+                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
+                        // exit the app if one permission is not granted
+                        Toast.makeText(this, "Required permission '" + permissions[index]
+                                + "' not granted, exiting", Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+                }
+                // all permissions were granted
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
--- android/app/src/main/java/com/toilatester/sms/listener/NettyServerService.java
+++ android/app/src/main/java/com/toilatester/sms/listener/NettyServerService.java
@@ -1,19 +1,26 @@
 package com.toilatester.sms.listener;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import com.toilatester.sms.MainActivity;
 import com.toilatester.sms.server.HttpServer;
 import com.toilatester.smslistener.R;
 
+import java.util.logging.Logger;
+
 public class NettyServerService extends Service {
-    private static final int NOTIFY_ID = 1;
-    private static final String NOTIFY_CHANNEL_ID = "Channel_Id";
+    private final Logger LOG = Logger.getLogger(HttpServer.class.getName());
+    public static final String CHANNEL_ID = "SMSListenerForegroundServiceChannel";
     private static HttpServer server;
 
     @Nullable
@@ -23,11 +30,28 @@ public class NettyServerService extends Service {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        LOG.info("NettyServerService created");
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("https://toilatester.blog")
+                .setContentText("SMSListenerService")
+                .setSmallIcon(R.mipmap.toilatester)
+                .setContentIntent(pendingIntent)
+                .setPriority(Notification.PRIORITY_HIGH)
+                .build();
+        startForeground(1, notification);
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (server == null) {
             int serverPort = intent.getIntExtra("serverPort", 8181);
-            server = new HttpServer(this.getApplicationContext(), this.getContentResolver(), serverPort);
-            server.startServer();
+            startNettyServer(serverPort);
         }
         return START_STICKY;
     }
@@ -35,9 +59,40 @@ public class NettyServerService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        stopNettyServer();
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+        LOG.warning("onTaskRemoved remove task");
+    }
+
+    private void startNettyServer(int serverPort) {
+        if (server != null)
+            return;
+        server = new HttpServer(this.getApplicationContext(), this.getContentResolver(), serverPort);
+        server.startServer();
+        LOG.info("Completed start Netty server");
+    }
+
+    private void stopNettyServer(){
         if (server != null) {
             server.stopServer();
             server = null;
+            LOG.warning("Stop Netty server");
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
         }
     }
 }
